aac177d6f5edcd76f3fca07fcae4234281f938c6
Randy Geraads <r.geraads@gmail.com>
2020-05-31 00:25:11 +0200

    /health controller

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class HealthController
+{
+    /**
+     * @Route("/health", methods={"GET"})
+     */
+    public function __invoke(): Response
+    {
+        return new Response('👍', 200);
+    }
+}
