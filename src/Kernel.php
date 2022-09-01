8273df6078502abeac152d8314b7f215453ef14e
Randy Geraads <randygeraads@Randys-MacBook-Pro.local>
2022-09-01 15:58:32 +0200

    add project dir override

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,12 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    /**
+     * Overrides the need of composer.json to be present in the repository, e.g. on production.
+     */
+    public function getProjectDir(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
