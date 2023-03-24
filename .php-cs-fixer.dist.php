0c8104fbd88f25135f46a776867c950468db3b22
Randy Geraads <randy.geraads@mailtopay.nl>
2023-03-24 10:16:23 +0100

    cs-fixer: no unused imports

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -12,6 +12,7 @@ return (new PhpCsFixer\Config())
         'yoda_style' => false,
         'php_unit_method_casing' => ['case' => 'snake_case'],
         'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
     ])
     ->setFinder($finder)
 ;
